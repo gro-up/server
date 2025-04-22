bea4e3e3b54e73860ae0415cc2c0a37753b40262
golden-hamster <isack19999@gmail.com>
2025-04-22 22:28:24 +0900

    Init

---
+++ src/main/java/com/hamster/gro_up/GroUpApplication.java
@@ -0,0 +1,13 @@
+package com.hamster.gro_up;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hamster/gro_up/GroUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamster.gro_up;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
