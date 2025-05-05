a9e0f010b809141a6d5fc5a94549c00bce2cab7b
golden-hamster <isack19999@gmail.com>
2025-05-05 16:03:10 +0900

    docs: `Auth`, `Company` Controller 문서화 (#25)

--- src/main/java/com/hamster/gro_up/controller/AuthController.java
+++ src/main/java/com/hamster/gro_up/controller/AuthController.java
@@ -5,6 +5,8 @@ import com.hamster.gro_up.dto.request.SigninRequest;
 import com.hamster.gro_up.dto.request.SignupRequest;
 import com.hamster.gro_up.dto.response.TokenResponse;
 import com.hamster.gro_up.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "인증 및 인가", description = "인증 및 인가 관련 API")
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
 @RestController
@@ -20,13 +23,15 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @Operation(summary = "일반 회원가입")
     @PostMapping("/signup")
     public ResponseEntity<ApiResponse<TokenResponse>> signup(@Valid @RequestBody SignupRequest signupRequest) {
         TokenResponse response = authService.signup(signupRequest);
         return ResponseEntity.ok(ApiResponse.ok(response));
     }
 
-    @PostMapping("/siginin")
+    @Operation(summary = "일반 로그인")
+    @PostMapping("/signin")
     public ResponseEntity<ApiResponse<TokenResponse>> signin(@Valid @RequestBody SigninRequest signinRequest) {
         TokenResponse response = authService.signin(signinRequest);
         return ResponseEntity.ok(ApiResponse.ok(response));
--- src/main/java/com/hamster/gro_up/controller/CompanyController.java
+++ src/main/java/com/hamster/gro_up/controller/CompanyController.java
@@ -7,6 +7,8 @@ import com.hamster.gro_up.dto.request.CompanyUpdateRequest;
 import com.hamster.gro_up.dto.response.CompanyListResponse;
 import com.hamster.gro_up.dto.response.CompanyResponse;
 import com.hamster.gro_up.service.CompanyService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "기업", description = "기업 관련 API")
 @RequiredArgsConstructor
 @RequestMapping("/api/companies")
 @RestController
@@ -21,24 +24,28 @@ public class CompanyController {
 
     private final CompanyService companyService;
 
+    @Operation(summary = "기업 단건 조회")
     @GetMapping("/{companyId}")
     public ResponseEntity<ApiResponse<CompanyResponse>> findCompany(@AuthenticationPrincipal AuthUser authUser, @PathVariable long companyId) {
         CompanyResponse response = companyService.findCompany(authUser, companyId);
         return ResponseEntity.ok(ApiResponse.ok(response));
     }
 
+    @Operation(summary = "해당 사용자의 모든 기업 조회")
     @GetMapping
     public ResponseEntity<ApiResponse<CompanyListResponse>> findAllCompany(@AuthenticationPrincipal AuthUser authUser) {
         CompanyListResponse response = companyService.findAllCompany(authUser);
         return ResponseEntity.ok(ApiResponse.ok(response));
     }
 
+    @Operation(summary = "기업 생성")
     @PostMapping
     public ResponseEntity<ApiResponse<CompanyResponse>> createCompany(@AuthenticationPrincipal AuthUser authUser, @Valid @RequestBody CompanyCreateRequest companyCreateRequest) {
         CompanyResponse response = companyService.createCompany(authUser, companyCreateRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.of(HttpStatus.CREATED, response));
     }
 
+    @Operation(summary = "기업 수정")
     @PutMapping("/{companyId}")
     public ResponseEntity<ApiResponse<Void>> updateCompany(@AuthenticationPrincipal AuthUser authUser,
                                                            @PathVariable long companyId,
@@ -47,6 +54,7 @@ public class CompanyController {
         return ResponseEntity.ok(ApiResponse.ok(null));
     }
 
+    @Operation(summary = "기엽 삭제")
     @DeleteMapping("/{companyId}")
     public ResponseEntity<ApiResponse<Void>> deleteCompany(@AuthenticationPrincipal AuthUser authUser,
                                                            @PathVariable long companyId) {
