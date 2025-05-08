d2a53bb510e94b4b8a0d869dfdc15aaf27b188de
golden-hamster <isack19999@gmail.com>
2025-05-08 23:13:47 +0900

    chore: 테스트용 api 시큐리티 허용 추가 (#10)

--- src/main/java/com/hamster/gro_up/config/SecurityConfig.java
+++ src/main/java/com/hamster/gro_up/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/oauth2/**", "/login/oauth2/**", "/api/auth/**", "/swagger-ui/**", "swagger-ui.html", "/v3/api-docs/**")
+                        .requestMatchers("/oauth2/**", "/login/oauth2/**", "/api/auth/**", "/swagger-ui/**", "swagger-ui.html", "/v3/api-docs/**", "/api/health/**")
                         .permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
