c1fe296f0d5340f9cf42ebb3443613f6ca0c75b0
golden-hamster <isack19999@gmail.com>
2025-05-13 20:54:09 +0900

    refactor: `user` 테이블의 `name` 속성 제거에 따른 코드 수정 (#55)

--- src/main/java/com/hamster/gro_up/dto/AuthUser.java
+++ src/main/java/com/hamster/gro_up/dto/AuthUser.java
@@ -13,13 +13,11 @@ public class AuthUser {
 
     private final Long id;
     private final String email;
-    private final String name;
     private final Collection<? extends GrantedAuthority> authorities;
 
-    public AuthUser(Long id, String email, String name, Role role) {
+    public AuthUser(Long id, String email, Role role) {
         this.id = id;
         this.email = email;
-        this.name = name;
         if (role == Role.ROLE_ADMIN) {
             this.authorities = List.of(
                     new SimpleGrantedAuthority(Role.ROLE_ADMIN.name()),
